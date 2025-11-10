209d3c3974095e85f3840d02d6549532b89d1d03
KMoon222 <ka539608@ucf.edu>
2025-11-09 22:57:19 -0500

    add proj

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject enemyOnePrefab;
+    public GameObject enemyTwoPrefab;
+
+    public float horizontalScreenSize;
+    public float verticalScreenSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        horizontalScreenSize = 10f;
+        verticalScreenSize = 6.5f;
+
+        InvokeRepeating("CreateEnemyOne", 1, 3);
+        InvokeRepeating("CreateEnemyTwo", 1, 8);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void CreateEnemyOne()
+    {
+        Instantiate(enemyOnePrefab, new Vector3(Random.Range(-9f, 9f), 6.5f, 0), Quaternion.identity);
+    }
+
+    void CreateEnemyTwo()
+    {
+        Instantiate(enemyTwoPrefab, new Vector3(Random.Range(-9f, 9f), 6.5f, 0), Quaternion.identity);
+    }
+}
